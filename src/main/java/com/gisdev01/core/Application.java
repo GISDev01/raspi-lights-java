08bce7084dec0b6472ed08426217f0b267526765
gisdev01 <gisdev01@gmail.com>
2017-12-09 16:19:07 -0500

    Moved controller mappings out into its' own file

--- src/main/java/com/gisdev01/core/Application.java
+++ src/main/java/com/gisdev01/core/Application.java
@@ -3,30 +3,11 @@ package com.gisdev01.core;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
+import com.gisdev01.model.RasPi;
 
 @RestController
 @EnableAutoConfiguration
 public class Application {
-
-    @RequestMapping("/")
-    String homepage() {
-        return "Web App Front end placeholder";
-    }
-
-    @RequestMapping(value = "/turnlightson", method = RequestMethod.POST)
-    public void turnLightsOn()
-    {
-        System.out.println("Turning lights on.");
-
-    }
-
-    @RequestMapping(value = "/turnlightsoff", method = RequestMethod.POST)
-    public void turnLightsOff()
-    {
-        System.out.println("Turning lights off.");
-
-    }
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
