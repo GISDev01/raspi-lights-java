dd46dbbd115eabbc821ee63321502f19b0e366ab
gisdev01 <gisdev01@gmail.com>
2017-12-17 17:55:00 -0500

    Create new Lightswitch model to track state changes based on UI interactionsˆ

---
+++ src/main/java/com/gisdev01/treelights/LightSwitch.java
@@ -0,0 +1,27 @@
+package com.gisdev01.treelights;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import javax.annotation.sql.DataSourceDefinition;
+
+@Data
+@Entity
+public class LightSwitch {
+    private @Id
+    @GeneratedValue Long id;
+    private String name;
+    private String state;
+
+    private LightSwitch() {}
+
+    public LightSwitch(String name, String state) {
+        this.name = name;
+        this.state = state;
+
+    }
+
+}
